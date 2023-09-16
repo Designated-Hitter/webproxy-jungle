ee85a87cbc87a410156fbf802077aed80c459b00
Designated-Hitter <dosadola@gmail.com>
2023-09-16 17:21:55 +0900

    adding comment and may support .mp4 format video

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -17,8 +17,8 @@ void serve_dynamic(int fd, char *filename, char *cgiargs);
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
 
 int main(int argc, char **argv) {
-  int listenfd, connfd;
-  char hostname[MAXLINE], port[MAXLINE];
+  int listenfd, connfd; //listen socket 과 connection socket 선언
+  char hostname[MAXLINE], port[MAXLINE]; //connection을 위한 hostname 과 port 선언
   socklen_t clientlen;
   struct sockaddr_storage clientaddr;
 
@@ -28,15 +28,15 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
-  listenfd = Open_listenfd(argv[1]);
+  listenfd = Open_listenfd(argv[1]); //listen socket open
 
   while (1) {
     clientlen = sizeof(clientaddr);
-    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);  // line:netp:tiny:accept
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);  // line:netp:tiny:accept 연결요청을 접수함
     Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
     printf("Accepted connection from (%s, %s)\n", hostname, port);
-    doit(connfd);   // line:netp:tiny:doit
-    Close(connfd);  // line:netp:tiny:close
+    doit(connfd);   // line:netp:tiny:doit transaction 실행
+    Close(connfd);  // line:netp:tiny:close 연결 닫기
   }
 }
 
@@ -53,32 +53,37 @@ void doit(int fd) {
   printf("Request headers:\n");
   printf("%s", buf);
   sscanf(buf, "%s %s %s", method, uri, version);
+  //GET이 아닌 메소드를 req했다면 error를 return 
   if (strcasecmp(method, "GET")) {
     clienterror(fd, method, "501", "Not implementd", "Tiny does not implement this method.");
     return;
   }
   
-  read_requesthdrs(&rio);
+  read_requesthdrs(&rio); //req headers 읽기
 
   //GET req로부터 URI parse
-  is_static = parse_uri(uri, filename, cgiargs);
+  is_static = parse_uri(uri, filename, cgiargs); //URI를 parse해서 정적/동적 컨텐츠인지 판단
+  //file이 disk에 없다면 error를 return
   if (stat(filename, &sbuf) < 0) {
     clienterror(fd, filename, "404", "Not found", "tiny couldn't find this file.");
     return;
   }
   
   if (is_static) { //정적 콘텐츠 제공
+    //(읽을 수 있는) 보통파일인지, 파일에 대한 읽기 권한을 가지고 있는지 판단
     if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
-      clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file.");
+      clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file."); //못 읽으면 error return
       return;
     }
-
+    //static content 제공
     serve_static(fd, filename, sbuf.st_size);
   } else { //동적 콘텐츠 제공
+    //CGI 프로그램을 실행할 수 없으면 error return
     if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) {
       clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't run the CGI program.");
       return;
     }
+    //dynamic content 제공
     serve_dynamic(fd, filename, cgiargs);
   }
 }
@@ -94,7 +99,7 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longms
   sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
 
   //HTTP res 출력하기
-  sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+  sprintf(buf, "HTTP/1.1 %s %s\r\n", errnum, shortmsg);
   Rio_writen(fd, buf, strlen(buf));
   sprintf(buf, "Content-type: text/html\r\n");
   Rio_writen(fd, buf, strlen(buf));
@@ -119,7 +124,7 @@ void read_requesthdrs(rio_t *rp) {
 int parse_uri(char *uri, char *filename, char *cgiargs) {
   char *ptr;
 
-  if (!strstr(uri, "cgi-bin")) { //정적 콘텐츠
+  if (!strstr(uri, "cgi-bin")) { //정적 콘텐츠: URI에 /cgi-bin을 포함하는 경로가 없다.
     strcpy(cgiargs, "");
     strcpy(filename, ".");
     strcat(filename, uri);
@@ -152,7 +157,7 @@ void serve_static(int fd, char *filename, int filesize) {
 
   //client에게 res headers 보내기
   get_filetype(filename, filetype);
-  sprintf(buf, "HTTP/1.0 200 OK\r\n");
+  sprintf(buf, "HTTP/1.1 200 OK\r\n");
   sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
   sprintf(buf, "%sConnection: close\r\n", buf);
   sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
@@ -178,9 +183,8 @@ void get_filetype(char *filename, char *filetype) { //filename 으로부터 file
     strcpy(filetype, "image/png");
   } else if (strstr(filename, ".jpg")) {
     strcpy(filetype, "image/jpeg");
-  //} else if (strstr(filename, ".mp4")) {
-    //strcpy(filetype, "???/mp4?")
-  //}
+  } else if (strstr(filename, ".mp4")) {
+    strcpy(filetype, "video/mp4");
   } else {
     strcpy(filetype, "text/plain");
   }
@@ -190,7 +194,7 @@ void serve_dynamic(int fd, char *filename, char *cgiargs) {
   char buf[MAXLINE], *emptylist[] = {NULL};
 
   //HTTP res의 first part return
-  sprintf(buf, "HTTP/1.0 200 OK\r\n");
+  sprintf(buf, "HTTP/1.1 200 OK\r\n");
   Rio_writen(fd, buf, strlen(buf));
   sprintf(buf, "Server: Tiny Web Server\r\n");
   Rio_writen(fd, buf, strlen(buf));
@@ -200,5 +204,5 @@ void serve_dynamic(int fd, char *filename, char *cgiargs) {
     Dup2(fd, STDOUT_FILENO); //redirect stdout to client
     Execve(filename, emptylist, environ); //CGI program 실행
   }
-  Wait(NULL); //자식 프로세스가 실행되고 결과를 출력할때까지 기다림
+  Wait(NULL); //자식 프로세스가 실행되고 결과를 출력하고 종료될 때까지 기다림
 }
